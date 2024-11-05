075aa983aa3bec9cee8a434f13204ccee3e27481
francids <francisco.mc.ids@gmail.com>
2024-11-05 15:03:32 -0400

    chore: Add initial project configuration with Vite, Unocss and SolidJS

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import UnocssPlugin from '@unocss/vite';
+
+export default defineConfig({
+  plugins: [
+    solidPlugin(),
+    UnocssPlugin({}),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
